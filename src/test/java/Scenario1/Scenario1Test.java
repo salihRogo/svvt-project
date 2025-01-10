37e1c9f18c3e005b16f631747cf1f2bf646290a4
ljirna <mirna.ljiljic@stu.ibu.edu.ba>
2025-01-10 19:50:10 +0100

    Test registration scenarion updated

--- src/test/java/Scenario1/Scenario1Test.java
+++ src/test/java/Scenario1/Scenario1Test.java
@@ -87,6 +87,61 @@ public class Scenario1Test {
         Thread.sleep(3000);
     }
 
+    @Test
+    public void testFailedRegistrationFunctionality() throws InterruptedException {
+        webDriver.get(baseUrl);
+        webDriver.manage().window().maximize();
+        Thread.sleep(2000);
+
+        WebElement signUpButton = webDriver.findElement(By.xpath("/html/body/div/header/div[1]/div/div/div/div/a[4]"));
+        signUpButton.click();
+        Thread.sleep(1000);
+
+        WebElement nameAndSurnameInputField = webDriver.findElement(By.id("user-name"));
+        nameAndSurnameInputField.sendKeys("Mirna Ljiljic");
+
+        js.executeScript("window.scrollBy(0,400)", "");
+
+        Thread.sleep(1000);
+
+        WebElement emailInputField = webDriver.findElement(By.id("user-email"));
+        emailInputField.sendKeys("ababababgmail.com");
+        Thread.sleep(1000);
+
+        WebElement phoneInputField = webDriver.findElement(By.id("user-phone"));
+        phoneInputField.sendKeys("063634799");
+        Thread.sleep(1000);
+
+        WebElement zipCodeInputField = webDriver.findElement(By.id("user-zip-code"));
+        zipCodeInputField.sendKeys("71000");
+        Thread.sleep(1000);
+
+        WebElement cityInputField = webDriver.findElement(By.id("user-city"));
+        cityInputField.sendKeys("Sarajevo");
+        Thread.sleep(1000);
+
+        WebElement addressInputField = webDriver.findElement(By.id("user-address"));
+        addressInputField.sendKeys("Tajna adresa");
+        Thread.sleep(1000);
+
+        WebElement passwordInputField = webDriver.findElement(By.id("user-password"));
+        passwordInputField.sendKeys("Mirnanemirna1");
+        Thread.sleep(1000);
+
+        WebElement repeatPasswordInputField = webDriver.findElement(By.id("user-password-confirmation"));
+        repeatPasswordInputField.sendKeys("Mirnanemirna1");
+        Thread.sleep(1000);
+
+        js.executeScript("window.scrollBy(0,200)", "");
+        Thread.sleep(1000);
+
+        WebElement submitSignUpButton = webDriver.findElement(By.xpath("/html/body/div[1]/main/div/div/div/div/div/div[2]/form/div[3]/button"));
+        submitSignUpButton.click();
+        Thread.sleep(4000);
+
+        assertEquals("https://www.univerzalno.com/bs/showRegistrationForm", webDriver.getCurrentUrl());
+        System.out.println("Registration couldn't be completed because of incorrect email form.");
+    }
 
     @AfterAll
     public static void tearDown() {
